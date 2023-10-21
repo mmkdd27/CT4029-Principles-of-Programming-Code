f48ab2f8fd746fb5990b8be2c6bac195b8bc2818
Mohammad Mehdi Kadkhodazadeh <mmkdd27@gmail.com>
2023-10-21 20:08:02 +0330

    day 2

---
+++ Day 2 - Session 3/traingle_calculator.py
@@ -0,0 +1,21 @@
+import math as math
+side1 = int(input("enter the length of one side : "))
+side2 = int(input("enter the length of another side : "))
+side3 = int(input("enter the length of the final side : "))
+s = (side1+side2+side3)//2
+print("area is : ",math.sqrt((side1+side2+side3)//2))
+
+if side1 == side2:
+    if side2 == side3:
+        print("the triangle is equilateral and isosceles since all sides are of the same length ")
+    else:
+        print("the triangle is is only isosceles since only two side are the same length ")
+else:
+    if side1 == side3:
+        print("the triangle is is only isosceles since only two side are the same length ")
+
+    elif side2 == side3:
+        print("the triangle is is only isosceles since only two side are the same length ")
+    else:
+        print("the triangle is scalene since none of the sides are of matching lengths ")
+
---
+++ Day 2 - Session 3/triangle_maker.py
@@ -0,0 +1,11 @@
+def func(rows,charachter):
+    for i in range(0,9):
+        for q in range(i+1,rows):
+            print(" ",end="")
+        for j in range(0,2*i+1):
+            print(charachter,end="")
+        print()
+
+
+
+
