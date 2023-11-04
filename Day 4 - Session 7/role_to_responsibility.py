72a7452032ac7daa3c26374a6e6b778839110de0
Mohammad Mehdi Kadkhodazadeh <mmkdd27@gmail.com>
2023-11-04 20:39:33 +0330

    still has issues with matching roles

--- Day 4 - Session 7/role_to_responsibility.py
+++ Day 4 - Session 7/role_to_responsibility.py
@@ -1,12 +1,24 @@
-###########reader1 = open("employee_data1.txt", "r")
-reader2 = open("employee_data2.txt", "r")
-employee_data1 = reader1.readlines()
-employee_data2 = reader2.readlines()
-separator = ","
-# do not set separator to "/" to avoid problems when it comes to date of birth
-for i in employee_data1:
-    full_name, contact_info, address, DoB, role1 = i.split(",")
-    print(role1)
-    for b in employee_data2:
-        responsibility, role2 = b.split(",")
-        print(role2)
+# Read employee personal information from file 1
+file1 = open('employee_data1.txt', "r")
+employee_info = file1.readlines()
+
+file2 = open("employee_data2.txt", "r")
+job_responsibilities = file2.readlines()
+
+roles_responsibilities = {}
+for line in job_responsibilities:
+    role, responsibility = line.strip().split("#")
+    roles_responsibilities[role] = responsibility
+
+# Match roles and print combined information in a separate text file
+output_file = open('combined_info.txt', 'w')
+for line in employee_info:
+    data = line.strip().split('&')
+    first_name, last_name, contact_info, address, dob, role = data
+    responsibility = roles_responsibilities.get(role, "Not specified")
+    combined_info = f"Name: {first_name} {last_name}\nContact Info: {contact_info}\nAddress: {address}\nDOB: {dob}\nRole: {role}\nResponsibility: {responsibility}\n"
+    output_file.write(combined_info)
+
+
+file1.close()
+file2.close()
