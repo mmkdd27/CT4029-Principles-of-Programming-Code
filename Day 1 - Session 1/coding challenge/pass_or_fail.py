f827a994f16f067968b339f5189b5aa70dca8d34
Mohammad Mehdi Kadkhodazadeh <mmkdd27@gmail.com>
2023-11-06 17:46:09 +0330

    added file

---
+++ Day 1 - Session 1/coding challenge/pass_or_fail.py
@@ -0,0 +1,6 @@
+while True:
+    grade = int(input("enter your grade: %"))
+    if grade >= 40:
+        print("pass")
+    if grade < 40:
+        print("fail")
---
+++ Day 1 - Session 1/coding challenge/positive_negative_checker.py
@@ -0,0 +1,7 @@
+num = int(input("enter your number"))
+if num > 0:
+    print("positive")
+if num == 0:
+    print("zero!")
+if num < 0:
+    print("negtive")
