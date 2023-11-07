e172f05be2d74caf27b1da4045744dcfa67e247b
Mohammad Mehdi Kadkhodazadeh <mmkdd27@gmail.com>
2023-11-07 15:48:10 +0330

    added extra files

--- Coding Challenges/Day 2 -  Coding Challenge/string_reveser.py
+++ Coding Challenges/Day 2 -  Coding Challenge/string_reveser.py
@@ -1,6 +1,6 @@
 def reverser(s):
-    str = ""
-    for i in s:
+    str = ""#I create a new empty string
+    for i in s: #i add every letter of the original string to the new one but from the left
         str = i + str
     return str
 
@@ -9,7 +9,7 @@ sring = input("enter your string: ")
 print(reverser(sring))
 
 
-#num1, num2, num3 = (input("enter your values sepertaed by a ','").split(","))
+num1, num2, num3 = (input("enter your values sepertaed by a ','").split(","))
 def maximizer(num1, num2, num3):
     if num1 >= num2 and num1 >= num3:
         if num1 == num2 == num3:
@@ -30,7 +30,7 @@ def maximizer(num1, num2, num3):
 
 
 def find_max_and_min(num1, num2, num3):
-    maximum = num1
+    maximum = num1#I assume num1 one to be minimum and maximum then I challenge it with comparing with other values
     minimum = num1
 
     if num2 > maximum:
