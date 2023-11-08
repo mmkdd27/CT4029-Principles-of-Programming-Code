57081ecc476756c55a8ae3f56ee210cf1288fbb2
Mohammad Mehdi Kadkhodazadeh <mmkdd27@gmail.com>
2023-11-08 18:22:46 +0330

    added files

---
+++ Coding Challenges/Day 3 - Coding Challenge/list_delete_change_add.py
@@ -0,0 +1,8 @@
+list1 = [1, "banana",2.45,True,8,5,(1,"oranges",["duren",4,7,3]),]
+print(list1)
+print(list1[2])
+del list1[-1]
+print(list1)
+del list1[0]
+print(list1)
+list1.append("6 is afird of 7")
