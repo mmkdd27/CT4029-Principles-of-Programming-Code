66070f3ec0194a0f4cc61e3f8b46c5ecf6f7cefc
Mohammad Mehdi Kadkhodazadeh <mmkdd27@gmail.com>
2023-11-10 12:32:04 +0330

    added files

---
+++ Coding Challenges/Day 5 - Coding Challenge/matrix_to_array.py
@@ -0,0 +1,14 @@
+import numpy as n
+matrix = [
+    [1, 2, 3],
+    [4, 8, 5],
+    [9, 5, 4]
+]
+
+Arr = np.array(matrix)
+print(Arr)
+
+new_shape = (1, -1)
+Arr_reshaped = Arr.reshape(new_shape)
+
+print(Arr_reshaped)
